5be64e1207d207cfe9cebd8bb8b0f63d7d3ea856
UENO, M. <eunosium-1128@live.jp>
2026-01-07 18:40:59 +0900

    Remove CMake version update command
    Remove sed command to update CMake version in bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,5 @@
 setlocal EnableDelayedExpansion
 
-sed -i "s/cmake_minimum_required(VERSION [0-9.]\+)/cmake_minimum_required(VERSION 3.5)/" CMakeLists.txt
-
 :: Make a build folder and change to it
 mkdir build
 cd build
